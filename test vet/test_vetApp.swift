629a9052d997576c246a0c725a5ff7b3a761ee4c
Daylyn  Kokeza <daylynkokeza@Daylyns-MacBook-Pro.local>
2024-12-08 20:18:12 -0500

    first commit with auto generated code from figma :)

---
+++ test vet/test_vetApp.swift
@@ -0,0 +1,17 @@
+//
+//  test_vetApp.swift
+//  test vet
+//
+//  Created by Daylyn  Kokeza  on 2024-03-06.
+//
+
+import SwiftUI
+// App Entry Point
+@main
+struct MyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            MainView()
+        }
+    }
+}
